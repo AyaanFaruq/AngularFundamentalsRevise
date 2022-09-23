77822b255537fd2509db5e36cd735c7007a29113
Faruque Reza <faruque@ptssolutions.com>
2022-09-23 12:07:32 +0600

    ng-for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name: string = 'Ayaan';
-  handleChange(event: any) {
-    // console.log(event);
-    this.name = event.target.value;
-  }
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+    },
+  ];
 }
