21a9ebf83dabfd601651d3d7e4cd194428f64ddc
Faruque Reza <faruque@ptssolutions.com>
2022-09-23 12:26:26 +0600

    container-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CommonModule,
-    FormsModule,
-    PassengerDashboardModule,
-  ],
-  providers: [],
+  imports: [BrowserModule, CommonModule, PassengerDashboardModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
